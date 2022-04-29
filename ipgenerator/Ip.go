ff5b6ef187e9375d522eb142c4258cb28e633263
michaelknudsen <mknudsen762@gmail.com>
2022-04-29 15:22:47 -0400

    switch from stupid ip struct to literally just an int

--- ipgenerator/Ip.go
+++ ipgenerator/Ip.go
@@ -1,4 +1,43 @@
 package ipgenerator
 
-type IpGenerator struct {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type IpGenerator uint32
+
+func (ipg *IpGenerator) ToString() string {
+	var sb strings.Builder
+	//printBytes(strconv.FormatInt(int64(*ipg), 2))
+
+	for i := 1; i <= 4; i++ {
+		//bit shift
+		shifted := int(*ipg) >> (8 * (4 - i))
+
+		//apply mask
+		mask := 255
+		masked := shifted & mask
+		sb.WriteString(strconv.FormatInt(int64(masked), 10))
+		if i <= 3 {
+			sb.WriteString(".")
+		}
+	}
+	return sb.String()
+}
+
+func printBytes(s string) {
+	//fmt.Println(s)
+	oct := 0
+	for i := 0; i < len(s); i++ {
+		fmt.Printf(string(s[i]))
+		if oct == 7 {
+			fmt.Printf(",")
+			oct = 0
+		} else {
+			oct++
+		}
+	}
+	fmt.Printf("\n")
 }
---
+++ ipgenerator/Ip_test.go
@@ -0,0 +1,43 @@
+package ipgenerator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_generator_default(t *testing.T) {
+	assert := assert.New(t)
+	ip := new(IpGenerator)
+	assert.Equal(fmt.Sprintf("%x", *ip), "0", "Initial ip should be zero")
+	*ip = 3232235521 //192.168.0.1 = 0a . 80 . 00 . 01
+}
+
+func Test_generator_actual_ip(t *testing.T) {
+	assert := assert.New(t)
+	ip := new(IpGenerator)
+	*ip = 3232235521 //192.168.0.1 = c0 . a8 . 00 . 01
+	assert.Equal(fmt.Sprintf("%x", *ip), "c0a80001", "Test ip 192.168.0.1 as hex")
+}
+func Test_ip_to_string(t *testing.T) {
+	assert := assert.New(t)
+	ip := new(IpGenerator)
+	*ip = 3232235521
+
+	assert.Equal("192.168.0.1", ip.ToString(), "print ip as string")
+}
+func Test_ip_to_string2(t *testing.T) {
+	assert := assert.New(t)
+	ip := new(IpGenerator)
+	*ip = 3232298295
+	assert.Equal("192.168.245.55", ip.ToString(), "print ip as string")
+}
+func Test_increment(t *testing.T) {
+	assert := assert.New(t)
+	ip := new(IpGenerator)
+	*ip = 3232235521
+	assert.Equal("192.168.0.1", ip.ToString(), "print ip as string")
+	*ip++
+	assert.Equal("192.168.0.2", ip.ToString(), "print ip as string")
+}
